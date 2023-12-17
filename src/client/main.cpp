250d4d7658ee32b2984e42913e4d72c16f8e2cd3
Demon1997rus <rotanov.1997@mail.ru>
2023-12-17 10:18:21 -0800

    try again

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,13 +1,75 @@
-#include <QCoreApplication>
-#include <QDir>
-#include <QString>
-#include <QDebug>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/socket.h>
+#include <errno.h>
 
-#include "processor.h"
+#define MAX_PAYLOAD 1024
 
-int main(int argc, char* argv[])
+int fd;
+sockaddr_nl src_addr;
+sockaddr_nl dest_addr;
+nlmsghdr* nlh = nullptr;
+iovec iov;
+msghdr msg;
+
+int main()
 {
-    QCoreApplication app(argc, argv);
-    new Processor(&app);
-    return app.exec();
+    fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+    std::cout << "Start client";
+    if (fd < 0)
+    {
+        std::cout << "Create socket failed";
+        return -1;
+    }
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = 1;
+    src_addr.nl_groups = 0;
+
+    // привязываем сокет к уникальному адресу
+    if (bind(fd, reinterpret_cast<sockaddr*>(&src_addr), sizeof(src_addr)) < 0)
+    {
+        std::cout << "Binding socket failed";
+        return -1;
+    }
+
+    // выделяем буфер для сообщения netlink, которое является заголовком сообщения + полезной нагрузкой сообщения
+    nlh = reinterpret_cast<nlmsghdr*>(malloc(NLMSG_SPACE(MAX_PAYLOAD)));
+
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = 1;
+    nlh->nlmsg_flags = 0;
+
+    strcpy(reinterpret_cast<char*>(NLMSG_DATA(nlh)), "Hello world!");
+
+    iov.iov_base = &nlh;
+    iov.iov_len = nlh->nlmsg_len;
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 2;
+    dest_addr.nl_groups = 0;
+
+    msg.msg_name = &dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    while (true)
+    {
+        sendmsg(fd, &msg, 0);
+        std::cout << "Send message" << (char*)NLMSG_DATA(nlh);
+    }
+    close(fd);
+    return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,69 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <linux/socket.h>
+#include <errno.h>
 
-int main(int argc, char* argv[])
+#define MAX_PAYLOAD 1024
+
+int fd;
+sockaddr_nl src_addr;
+sockaddr_nl dest_addr;
+nlmsghdr* nlh = nullptr;
+iovec iov;
+msghdr msg;
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+    if (fd < 0)
+    {
+        std::cout << "Create socket failed";
+        return -1;
+    }
 
-    return a.exec();
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = 2;
+    src_addr.nl_groups = 0;
+
+    if (bind(fd, reinterpret_cast<sockaddr*>(&src_addr), sizeof(src_addr)) < 0)
+    {
+        std::cout << "Binding socket failed";
+        return -1;
+    }
+
+    nlh = reinterpret_cast<nlmsghdr*>(malloc(NLMSG_SPACE(MAX_PAYLOAD)));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = 1000;
+    nlh->nlmsg_flags = 0;
+
+    iov.iov_base = &nlh;
+    iov.iov_len = nlh->nlmsg_len;
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_groups = 0;
+    dest_addr.nl_pid = 1;
+
+    msg.msg_name = &dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    while (true)
+    {
+        recvmsg(fd, &msg, 0);
+        std::cout << "Received message:" << reinterpret_cast<char*>(NLMSG_DATA(nlh));
+    }
+    close(fd);
+    return 0;
 }
